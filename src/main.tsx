5d9b287e5d6c27ce1994674e69c1124b2d7456ca
basedhypestyle-sketch <basedhypestyle@gmail.com>
2025-11-21 07:22:30 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { WagmiConfig } from 'wagmi'
-import { config } from './wagmi'
-import App from './app'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { WagmiConfig } from "wagmi";
+import { config } from "./wagmi";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WagmiConfig client={config}>
+    <WagmiConfig config={config}>
       <App />
     </WagmiConfig>
   </React.StrictMode>
-)
+);
